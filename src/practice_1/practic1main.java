81787f5c5d7171688a2fd7f8272fc18f3de82877
dj <dj@naver.com>
2021-08-28 23:03:43 +0900

    commit is save

--- src/practice_1/practic1main.java
+++ src/practice_1/practic1main.java
@@ -12,6 +12,7 @@ public class practic1main {
 		System.out.println(num1 + " + " + num2 + " = " +num1+num2);
 		System.out.println(num1 + " + " + num2 + " = " +(num1+num2));
 		
+		num1 = 3;
 		
 	}
 
