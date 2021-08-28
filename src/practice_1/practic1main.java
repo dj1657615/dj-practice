8933b84cc362455e569a0c2607158c0a6c7d4fb8
dj <dj@naver.com>
2021-08-28 23:13:43 +0900

    comflict test

--- src/practice_1/practic1main.java
+++ src/practice_1/practic1main.java
@@ -12,7 +12,12 @@ public class practic1main {
 		System.out.println(num1 + " + " + num2 + " = " +num1+num2);
 		System.out.println(num1 + " + " + num2 + " = " +(num1+num2));
 		
-		num1 = 3;
+		
+		num1 = 7;
+		num2 = 2;
+		sum = num1 + num2; 
+		System.out.println(num1 + " + " + num2 + " = " +num1+num2);
+		System.out.println(num1 + " + " + num2 + " = " +(num1+num2));
 		
 	}
 
