ae3d462df81dd5e1bbeea94bbe4494686d717921
dj <dj@naver.com>
2021-08-28 22:59:29 +0900

    add 5 + 10 print source code

---
+++ src/practice_1/practic1main.java
@@ -0,0 +1,18 @@
+package practice_1;
+
+public class practic1main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!!");
+		int sum, num1,num2;
+		num1 = 5 ;
+		num2 = 10;
+		sum = num1 + num2; 
+		System.out.println(num1 + " + " + num2 + " = " +num1+num2);
+		System.out.println(num1 + " + " + num2 + " = " +(num1+num2));
+		
+		
+	}
+
+}
